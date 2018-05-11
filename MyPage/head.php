9146dddd051f82d1e815b97a0bb9af28cbdba8a3
JoyzShnoPB <evgenij.jakushev@privatbank.ua>
2018-05-11 17:44:12 +0300

    апдейт

---
+++ MyPage/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Шапка</title>
+    <link href="css/myHEADcss.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<a href="MyPage.php" class="layer"</a> <img src="img/1.jpg" width="250px" height="150px">
+<table>
+    <tr>
+        <td>
+            <a href="index.html">Главная</a>
+        </td>
+        <td>
+            <a href="холодильник.php">Холодильник</a>
+        </td>
+        <td>
+            <a href="Рецепты.php">Рецепты</a>
+        </td>
+        <td>
+            <a href="Другое.php">Другое</a>
+        </td>
+    </tr>
+</table>
+
+
+<?php
+
+?>
+</body>
+</html>
---
+++ MyPage/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Моя Страничка</title>
+    <link href="css/mycss.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+
+<!--
+<?php
+error_reporting(-1);
+?>
+-->
+
+
+<!-- <img src="a.png" width="100%"> -->
+<table>
+    <tr>
+        <td bgcolor="aqua">
+            ячейка1
+        </td>
+        <td bgcolor="#8a2be2">
+            ячейчка2
+        </td>
+        <td bgcolor="#deb887">
+            ячейка3
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#7fff00">
+            ячейка4
+        </td>
+        <td bgcolor="#008b8b">
+            ячейчка5
+        </td>
+        <td bgcolor="#ff1493">
+            ячейка6
+        </td>
+    </tr>
+</table>
+<hr>
+<!--
+
+<?php
+include "head.php";
+$link = mysqli_connect('localhost', 'root', "", 'ForMe');
+$res = mysqli_query($link, "SELECT * FROM `Блюда`");
+print_r ($res,1);
+?>
+
+-->
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
